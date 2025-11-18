af86921089b8c32493d39d6c0828791fe4025888
Edward Brown <innovative.fowler@mask.pro.fmtr.dev>
2025-11-18 11:26:54 +0000

    Implement singular `fmtr-dns` entrypoint

---
+++ fmtr/dns/entrypoint.py
@@ -0,0 +1,7 @@
+def main():
+    from fmtr.dns.settings import settings
+    return settings.run()
+
+
+if __name__ == '__main__':
+    main()
--- fmtr/dns/settings.py
+++ fmtr/dns/settings.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from fmtr.dns.paths import paths
 from fmtr.dns.proxy import AdBlockDoHProxy
 from fmtr.tools import sets
@@ -6,3 +8,25 @@ from fmtr.tools import sets
 class Settings(sets.Base):
     paths = paths
     server: AdBlockDoHProxy
+
+    def run(self):
+        super().run()
+
+        from fmtr.tools import debug
+        debug.trace()
+        from fmtr import tools
+        from fmtr.dns.obs import logger
+        from fmtr.dns.paths import paths
+        from fmtr.dns.version import __version__
+
+        logger.info(f'Launching {paths.name_ns} {__version__=} {tools.get_version()=} from entrypoint.')
+        logger.debug(f'{paths.settings.exists()=} {str(paths.settings)=}')
+
+        logger.info(f'Launching server...')
+        from fmtr.dns.api import DNS
+
+        asyncio.run(DNS.start(settings.server))
+
+
+settings = Settings()
+settings
